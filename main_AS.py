407165fb18e09fe0a0e0dad104522525bdd647e9
anapatricias <anapatriciaportelinha@gmail.com>
2025-07-03 13:47:53 +0100

    Alt2

--- main_AS.py
+++ main_AS.py
@@ -21,9 +21,44 @@ try:
 
     print(f"O veículo circulava no local {loc} a {vel} km/h.")
 
-
-
-
 except ValueError:
     print('Introduza um valor válido!')
+    
+    #Função para calcular multa
+    
+def calcular_multa (velocidade,local):
+    multa = 0
+    
+    if velocidade < 50:
+        print("Sem multa. Velocidade abaixo de 50 km/h.")
+        return
+    
+    if local == 1 :
+        if velocidade >= 120:
+            multa = 320
+        elif velocidade >= 90:
+            multa = 120
+        elif velocidade > 50:
+            multa = 60
+    
+    elif local == 2:
+        if velocidade >=120:
+            multa = 120
+        elif velocidade >90:
+            multa = 60
+            
+    elif local == 3:
+        if velocidade > 175:
+            multa = 360
+        elif velocidade > 150:
+            multa * 120
+        elif velocidade > 120:
+            multa = 60
+    if multa > 0:
+        print (f"Multa de {multa} €")
+    else:
+        print("Sem multa.")
+        
+    def calcular_multa(velocidade, local):
+        
     
