8cc7c55e58fa9908e8f98d446340304e0cb3eabd
anapatricias <anapatriciaportelinha@gmail.com>
2025-07-03 13:37:13 +0100

    Revisão1

--- main_AS.py
+++ main_AS.py
@@ -1,4 +1,5 @@
 #Escolha do local da circulação
+
 try:
     print('\n Onde circulava o veiculo?')
     print('Escolha uma opção:')
@@ -12,8 +13,17 @@ try:
     if loc not in {1,2,3}:
         raise ValueError('Introduza um valor válido!')
     
-    print(f"O veículo circulava no local {loc}.")
+    print(f"O veículo circulava no local {loc}.")     
     
+    #Velocidade
+
+    vel = int(input('Introduza a velocidade do veículo (km/h):'))
+
+    print(f"O veículo circulava no local {loc} a {vel} km/h.")
+
+
+
+
 except ValueError:
-    print ('Introduza um valor válido!')
+    print('Introduza um valor válido!')
     
