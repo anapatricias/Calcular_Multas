c69f3e4d65b84dc767529209f38f701cc92db959
anapatricias <anapatriciaportelinha@gmail.com>
2025-07-03 14:08:27 +0100

    Alt4

--- main_AS.py
+++ main_AS.py
@@ -1,31 +1,6 @@
-#Escolha do local da circulação
 
-try:
-    print('\n Onde circulava o veiculo?')
-    print('Escolha uma opção:')
-    print('1-Localidade')
-    print('2-Fora da Localidade')
-    print('3-Autoestrada')
-    print()
-    
-    loc = int(input('Introduza o local (1/2/3):'))
-    
-    if loc not in {1,2,3}:
-        raise ValueError('Introduza um valor válido!')
-    
-    print(f"O veículo circulava no local {loc}.")     
-    
-    #Velocidade
-
-    vel = int(input('Introduza a velocidade do veículo (km/h):'))
-
-    print(f"O veículo circulava no local {loc} a {vel} km/h.")
-
-except ValueError:
-    print('Introduza um valor válido!')
-    
     #Função para calcular multa
-    
+
 def calcular_multa (velocidade,local):
     multa = 0
     
@@ -61,8 +36,8 @@ def calcular_multa (velocidade,local):
         print("Sem multa.")
         
         
-while True:
-     try:
+
+    try:
             print ('\n Onde circulava o veículo?')
             print ('Escolha uma opção:')
             print ('1-Localidade')
@@ -76,14 +51,14 @@ while True:
                 raise ValueError ('Introduza um valor válido')
             
             vel = int (input ('Introduza a velocidade do veículo (km/h):'))
-            
+   
+    while True:
             calcular_multa (vel,loc)
             
             if input('\n Deseja continuar? (s/n): '). lower () =='n':
                 break
         
-     except ValueError:
-            print ('Introduza um valor válido!')
-            input('Pressione qualquer tecla para continuar.')
+except ValueError:
+  print ('Introduza um valor válido!')
+          
             
-    
